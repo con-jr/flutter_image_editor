120f016a480d9945de8a1d85db22a379d82ec516
con-jr <clabaojr@gmail.com>
2020-12-10 20:28:42 +0800

    Update ImageHandler.kt
    remove subtraction of text.x

--- android/src/main/kotlin/top/kikt/flutter_image_editor/core/ImageHandler.kt
+++ android/src/main/kotlin/top/kikt/flutter_image_editor/core/ImageHandler.kt
@@ -148,13 +148,13 @@ class ImageHandler(private val context: Context, private var bitmap: Bitmap) {
       } catch (e: Exception) {
       }
     }
-    canvas.drawText(text.text, text.x.toFloat(), (text.y - text.height).toFloat(), textPaint)
+//    canvas.drawText(text.text, text.x.toFloat(), text.y.toFloat(), textPaint)
 
-//    val staticLayout = getStaticLayout(text, textPaint, canvas.width - text.x)
+    val staticLayout = getStaticLayout(text, textPaint, canvas.width)
 
-    canvas.translate(text.x.toFloat(), (text.y - text.height).toFloat())
-//    staticLayout.draw(canvas)
-    canvas.translate((-text.x).toFloat(), (-text.y - text.height).toFloat())
+    canvas.translate(text.x.toFloat(), text.y.toFloat())
+    staticLayout.draw(canvas)
+    canvas.translate((-text.x).toFloat(), (-text.y).toFloat())
   }
 
   @Suppress("DEPRECATION")
